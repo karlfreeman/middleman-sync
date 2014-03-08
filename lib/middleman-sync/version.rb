56bc9553f765727e784268b49190aefaeaa06e11
Karl Freeman <karlfreeman@gmail.com>
2014-03-08 10:39:37 +0000

    be clear there might be more than one .pre

--- lib/middleman-sync/version.rb
+++ lib/middleman-sync/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   module Sync
-    VERSION = '4.0.0.pre' unless defined?(Middleman::Sync::VERSION)
+    VERSION = '4.0.0.pre.0' unless defined?(Middleman::Sync::VERSION)
   end
 end
